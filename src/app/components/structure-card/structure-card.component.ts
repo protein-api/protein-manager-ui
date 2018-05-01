cd1c4c7b0e34030e25cf32d2381ebcf66150c7f5
matiazzz <matias.zacarias.e@gmail.com>
2018-05-01 14:49:47 -0300

    se agrega pv para visualizar pdbs

--- src/app/components/structure-card/structure-card.component.ts
+++ src/app/components/structure-card/structure-card.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core'
 
-declare const jQuery:any;
-declare const $:any;
+declare const jQuery:any
+declare const $:any
+
+declare const pv:any
 
 @Component({
   selector: 'app-structure-card',
@@ -18,8 +20,19 @@ export class StructureCardComponent implements OnInit {
   }
 
   ngOnInit() {
-    $('.modal').modal();
-    this.structureLink = this.getSructureLink(this.structure);
+    console.log(pv)
+    $('.modal').modal()
+    this.structureLink = this.getSructureLink(this.structure)
+
+    const options = {
+      width: 600,
+      height: 600,
+      antialias: true,
+      quality : 'medium'
+  }
+  
+  const viewer = pv.Viewer(document.getElementById('viewer'), options)
+  this.loadPdb(viewer, "1r6a")
   }
 
   getSructureLink = (structure) => {
@@ -28,16 +41,23 @@ export class StructureCardComponent implements OnInit {
     return link + struc
   }
 
-  openReactionModal = () => {
-    $('#modal-structure-' + this.structure).modal('open')
-    // this.loadPdb({
-    //   width: 600,
-    //   height: 600,
-    //   antialias: true,
-    //   quality : 'medium'
-    // });
-  }
+  openReactionModal = () => $('#modal-structure-' + this.structure).modal('open')
 
-  closeReactionModal = () => { $('#modal-structure-' + this.structure).modal('close') }
+  closeReactionModal = () => $('#modal-structure-' + this.structure).modal('close')
+
+  loadPdb = (viewer, pdbName) => {
+    pv.io.fetchPdb('/assets/pv/pdbs/' + pdbName + '.pdb', (structure) => {
+      // display the protein as cartoon, coloring the secondary structure
+      // elements in a rainbow gradient.
+      viewer.cartoon('protein', structure, { color : pv.color.ssSuccession() })
+      // there are two ligands in the structure, the co-factor S-adenosyl
+      // homocysteine and the inhibitor ribavirin-5' triphosphate. They have
+      // the three-letter codes SAH and RVP, respectively. Let's display them
+      // with balls and sticks.
+      const ligands = structure.select({ rnames : ['SAH', 'RVP'] })
+      viewer.ballsAndSticks('ligands', ligands)
+      viewer.centerOn(structure)
+  })
+  }
 
 }
